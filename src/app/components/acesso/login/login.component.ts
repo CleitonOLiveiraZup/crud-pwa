30c18c37c502af6359f35907e2591684cffc9f7a
CleitonOliveiraZup <cleiton.oliveira@zup.com.br>
2021-05-26 16:13:01 -0300

    login em processo

--- src/app/components/acesso/login/login.component.ts
+++ src/app/components/acesso/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import { faTruckMoving } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  faTruckMoving = faTruckMoving;
   constructor() { }
 
   ngOnInit(): void {
